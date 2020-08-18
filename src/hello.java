1ec74f4ee6c889e4af05eb256384c73bdc0b1084
EunJung <eunjungjang@kakao.com>
2020-08-18 21:53:40 +0900

    자바 연습예제 풀이

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HELLO, WORLD!");
+		System.out.println("Hello, /* 이것은 주석 아님 */ world.");
+		System.out.println("Hello, world. // 이것도 주석 아님");
+	}
+
+}
